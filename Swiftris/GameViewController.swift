b2da529b69bbea2b0afa52db507c91840d20ee1a
Shayne Tremblay <shaynetremblay@hotmail.com>
2015-08-23 10:26:24 -0400

    first

---
+++ Swiftris/GameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GameViewController.swift
+//  Swiftris
+//
+//  Created by Shayne Tremblay on 8/23/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}
